8d608f2d4ab3c372b5deafe9d2580977f20b2497
muhammadfariz123 <warungkombas@gmail.com>
2026-08-08 23:34:48 +0700

    fix: create vercel tmp storage subdirectories

--- api/index.php
+++ api/index.php
@@ -1,5 +1,15 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
+
+$storage = '/tmp/storage';
+if (!is_dir($storage)) {
+    mkdir($storage, 0777, true);
+    mkdir($storage . '/framework/cache/data', 0777, true);
+    mkdir($storage . '/framework/views', 0777, true);
+    mkdir($storage . '/framework/sessions', 0777, true);
+    mkdir($storage . '/logs', 0777, true);
+}
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
-$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+$app->useStoragePath($storage);
 $app->handleRequest(Illuminate\Http\Request::capture());
