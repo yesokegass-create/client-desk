1307912362c90a00647e8138008aa0ed4dc1f4ea
muhammadfariz123 <warungkombas@gmail.com>
2026-08-08 23:12:33 +0700

    fix: vercel read-only storage path

--- api/index.php
+++ api/index.php
@@ -1,3 +1,5 @@
 <?php
-
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+$app->handleRequest(Illuminate\Http\Request::capture());
