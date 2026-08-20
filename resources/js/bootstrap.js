f21d9b8df64b2c879b288809f694cc26e73ab6c4
muhammadfariz123 <warungkombas@gmail.com>
2026-08-21 06:55:56 +0700

    Add global Axios interceptors for Authorization and 401 handling

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,28 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Accept'] = 'application/json';
+
+// Add a request interceptor to attach the token
+window.axios.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+});
+
+// Add a response interceptor to handle 401 errors
+window.axios.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    if (error.response && (error.response.status === 401 || error.response.status === 419)) {
+        // Token is invalid or expired
+        localStorage.removeItem('auth_token');
+        localStorage.removeItem('vender_user_profile');
+        window.location.href = '/login';
+    }
+    return Promise.reject(error);
+});
